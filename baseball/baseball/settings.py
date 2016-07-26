3f170994dac708f6317dfbe7945d6c37ff48475a
seul lee <blacksangi14@naver.com>
2016-07-26 16:17:19 +0900

    no message

--- baseball/baseball/settings.py
+++ baseball/baseball/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'university',
+    'amateur',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -106,7 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
+#TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
