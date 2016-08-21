8768ca4dbc900e68af60b3bb5475aee374c392d2
seul lee <blacksangi14@naver.com>
2016-08-21 21:31:09 +0900

    no message

--- DjangoProject/university/urls.py
+++ DjangoProject/university/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from university import views
 
 urlpatterns = [
-#    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
         
 ]
--- DjangoProject/university/views.py
+++ DjangoProject/university/views.py
@@ -1,8 +1,15 @@
+# coding: utf-8
+
 from django.shortcuts import render
 from university.models import *
 from django.views.generic import ListView, DetailView
 
-#class IndexView(ListView):
-#    template_name = "university/index.html"
-#    context
+class IndexView(DetailView):
+    """
+    generic view를 이용
+    university 관련 플레이어의 성적 보여주기
+    """
+    model = Player
+    template_name = "university/index.html"
+
 
