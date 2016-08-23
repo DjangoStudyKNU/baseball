373fd176d2d02a813dc53e9d011641b36c701b22
seul lee <blacksangi14@naver.com>
2016-08-23 17:42:19 +0900

    add some views

--- DjangoProject/university/urls.py
+++ DjangoProject/university/urls.py
@@ -3,5 +3,7 @@ from university import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-      
+    url(r'^league/(?P<pk>\d+)/$', views.LeagueDetailView.as_view(), name='league_detail'),
+    url(r'^team/(?P<pk>\d+)/$', views.TeamDetailView.as_view(), name='team_detail'),
+    url(r'^uni_info/(?P<pk>\d+)/$', views.UniversityDetailView.as_view(), name='university_detail'),
 ]
