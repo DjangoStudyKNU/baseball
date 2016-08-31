8002a6795a52dfcd75ff146cfb78fede0a40a37c
Daewon Seo <saz0549@naver.com>
2016-08-31 16:50:26 +0900

    no message

--- DjangoProject/baseball/urls.py
+++ DjangoProject/baseball/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^university/', include('university.urls', namespace="university")),
-#    url(r'^amateur/', include('amateur.urls', namespace="amateur")),
+    url(r'^amateur/', include('amateur.urls', namespace="amateur")),
 
 ]
 
