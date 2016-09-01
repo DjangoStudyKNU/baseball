4906a98f92adef99506de73e7953fd5c02827c4d
Daewon Seo <saz0549@naver.com>
2016-09-01 21:15:20 +0900

    add login, login out view function

--- DjangoProject/amateur/views.py
+++ DjangoProject/amateur/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from amateur.forms import SignupForm, AuthenticationForm
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 
-# Create your views here.
+
+def login(request):
+
+    """
+    로그인 과정을 위한 폼입니다
+    """
+
+    var = {} # 변수를 렌더하기 위한 dictionary
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = authenticate(email=request.POST['email'], password=request.POST['password'])
+            if user is not None:
+                if user.is_active:
+                    django_login(request, user)
+                    return redirect('/')
+
+    else:
+        form = AuthenticationForm()
+        var['content'] = form
+
+    return render(request, 'amateur/login.html',  var)
+
+
+def logout(request):
+
+    django_logout(request)
+
+    return redirect('/')
+
